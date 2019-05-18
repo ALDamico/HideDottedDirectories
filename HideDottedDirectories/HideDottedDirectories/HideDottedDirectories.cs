e5941f9266fdc2e3b658e6fe93e36f019fbd80dd
Andrea Luciano Damico <33159753+ALDamico@users.noreply.github.com>
2019-05-18 15:46:48 +0200

    Service working as indended

---
+++ HideDottedDirectories/HideDottedDirectories/HideDottedDirectories.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace HideDottedDirectories
+{
+    public partial class HideDottedDirectories : ServiceBase
+    {
+        public HideDottedDirectories()
+        {
+            InitializeComponent();
+            InitializeFields();
+            SetupPoller();
+        }
+
+        private void InitializeFields()
+        {
+            timer = new Timer();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void SetupPoller()
+        {
+            timer.Interval = 60000;
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+            
+            RenameFolders();
+        }
+
+        private void RenameFolders()
+        {
+            var directories = EnumerateFolders();
+            foreach (var dir in directories)
+            {
+                File.SetAttributes(dir, FileAttributes.Hidden);
+            }
+
+        }
+
+        private IEnumerable<string> EnumerateFolders()
+        {
+            Directory.SetCurrentDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+            var directories = Directory.GetDirectories(Directory.GetCurrentDirectory(), "*", SearchOption.TopDirectoryOnly).ToList();
+            for (int i = 0; i < directories.Count; i++)
+            {
+                directories[i] = Path.GetFileName(directories[i]);
+            }
+            var dottedDirectories = (
+                    from directory 
+                    in directories
+                    where Regex.IsMatch(directory, @"^\..*")
+                    select directory
+                                    ).ToList();
+
+            return dottedDirectories;
+
+        }
+
+        private Timer timer;
+    }
+}
---
+++ HideDottedDirectories/HideDottedDirectories/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace HideDottedDirectories
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // ServiceProcessInstaller
+            // 
+            this.ServiceProcessInstaller.Password = null;
+            this.ServiceProcessInstaller.Username = null;
+            // 
+            // serviceInstaller
+            // 
+            this.serviceInstaller.Description = "Sets directories starting with a dot as hidden to emulate how they behave in *NIX" +
+    "es.";
+            this.serviceInstaller.DisplayName = "Hide dotted directories";
+            this.serviceInstaller.ServiceName = "HideDottedDirectories";
+            this.serviceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.ServiceProcessInstaller,
+            this.serviceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller ServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller;
+    }
+}
